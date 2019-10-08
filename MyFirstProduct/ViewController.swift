1f506257df213d0fadc1db6e64417c1254b71786
Maria <45818242+ma-ruba@users.noreply.github.com>
2019-10-08 14:23:10 +0300

    Add files via upload

--- MyFirstProduct/ViewController.swift
+++ MyFirstProduct/ViewController.swift
@@ -14,8 +14,16 @@ class ViewController: UIViewController {
     var firstNum: Double = 0;
     var mathSign: Bool = false;
     var operation: Int = 0;
+    var flag: Int = 0;
+    var operationNum: Int = 0;
+    var dotNum = false;
+    var equalOper = false;
     @IBOutlet weak var result: UILabel!
     @IBAction func digits(_ sender: UIButton) {
+        if equalOper {
+            result.text = ""
+            equalOper = false
+        }
         if mathSign == true {
             result.text = String(sender.tag)
             mathSign = false
@@ -23,12 +31,31 @@ class ViewController: UIViewController {
         else {
             result.text = result.text! + String(sender.tag)
         }
-        numberFromScreen = Double(result.text!)!
+        if Double(result.text!) != nil {
+            numberFromScreen = Double(result.text!)!
+        }
     }
-    
+    func equality () { //равно
+        operationNum = 0
+        if operation == 11 {
+            result.text = String(firstNum / numberFromScreen)
+        }
+        else if operation == 12 {
+            result.text = String(firstNum * numberFromScreen)
+        }
+        else if operation == 13 {
+            result.text = String(firstNum - numberFromScreen)
+        }
+        else if operation == 14 {
+            result.text = String(firstNum + numberFromScreen)
+        }
+    }
     @IBAction func buttons(_ sender: UIButton) {
         if result.text != "" && sender.tag != 10 && sender.tag != 15 {
-            if Double(result.text!) != nil {
+            if operationNum == 1 && sender.tag != 18 {
+                equality()
+            }
+            if Double(result.text!) != nil && sender.tag != 18{
                 firstNum = Double(result.text!)!
             }
             if sender.tag == 11 { //поделить
@@ -43,22 +70,35 @@ class ViewController: UIViewController {
             else if sender.tag == 14 {//прибавить
                 result.text = "+"
             }
-            operation = sender.tag
-            mathSign = true;
+            else if sender.tag == 17 {//процент числа
+                result.text = String(firstNum / 100)
+            }
+            else if sender.tag == 16 {//знак числа
+                flag += 1
+                if flag % 2 != 0 {
+                    result.text = "-" + result.text!
+                }
+                else {
+                    result.text?.removeFirst(1)
+                }
+            }
+            else if sender.tag == 18 && !dotNum {//запятая
+                if Double(result.text!) != nil {
+                    result.text = result.text! + "."
+                    dotNum = true
+                    numberFromScreen = Double(result.text!)!
+                }
+            }
+            if sender.tag != 18 {
+                mathSign = true
+                operation = sender.tag
+                operationNum = 1
+                dotNum = false
+            }
         }
         else if sender.tag == 15 { //равно
-            if operation == 11 {
-                result.text = String(firstNum / numberFromScreen)
-            }
-            else if operation == 12 {
-                result.text = String(firstNum * numberFromScreen)
-            }
-            else if operation == 13 {
-                result.text = String(firstNum - numberFromScreen)
-            }
-            else if operation == 14 {
-                result.text = String(firstNum + numberFromScreen)
-            }
+            equality()
+            equalOper = true
         }
         else if sender.tag == 10 {
             result.text = ""
